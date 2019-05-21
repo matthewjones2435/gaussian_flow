89267d39b79f9535dfa49a02f3c069d4cc00eb33
matthewjones <matthewjones2435@gmail.com>
2019-05-21 10:33:46 -0600

    Added ability to reject non integer values as input.

--- src/edu/cnm/deepdive/GaussianSum.java
+++ src/edu/cnm/deepdive/GaussianSum.java
@@ -4,28 +4,33 @@ import java.util.Scanner;
 
 public class GaussianSum {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int upperLimit;
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int upperLimit;
 
-        upperLimit = -1;
-        long sum;
-        String invalidInput;
-        
-        invalidInput = "Invalid input! Please try a number between 1 and 1000000";
-        while (upperLimit < 1 || upperLimit > 1000000) {
-            upperLimit = input.nextInt();
-            if (upperLimit < 1 || upperLimit > 1000000) {
-                System.out.println(invalidInput);
-            }
-        }
-        sum = (long) upperLimit * (upperLimit + 1) / 2;
-        String info;
-        
-        info = "The sum of 1 to ";
-        String is;
-        
-        is = " is ";
-        System.out.println(info + upperLimit + is + sum);
+    upperLimit = -1;
+    long sum;
+    String invalidInput;
+
+    invalidInput = "Invalid input! Please try a number between 1 and 1000000";
+    while (upperLimit < 1 || upperLimit > 1000000) {
+      while (!input.hasNextInt()) {
+        input.next();
+        System.out.println("Invalid input! please enter an integer number. ");
+      }
+
+      upperLimit = input.nextInt();
+      if (upperLimit < 1 || upperLimit > 1000000) {
+        System.out.println(invalidInput);
+      }
     }
+    sum = (long) upperLimit * (upperLimit + 1) / 2;
+    String info;
+
+    info = "The sum of 1 to ";
+    String is;
+
+    is = " is ";
+    System.out.println(info + upperLimit + is + sum);
+  }
 }
